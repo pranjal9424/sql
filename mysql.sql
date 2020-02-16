6483e59bb4d87450d72c35de2bf0af0aab36501d
pranjal9424 <pranjalgupta9424@gmail.com>
2020-02-16 19:23:38 +0530

    second commit

---
+++ mysql.sql
@@ -0,0 +1,69 @@
+show databases;
+
+use classicmodels;
+show tables;
+
+/* 
+SELECT statement that allows you to query data from a single table.
+If you have two or more statements, you use the semicolon ( ;) to separate them so that MySQL will execute each statement individually.
+MySQL evaluates the FROM clause first and then the SELECT clause:
+ */
+ 
+ select * from employees;
+ select * from customers;
+ select * from orderdetails;
+ select * from orders;
+ select * from products;
+ select * FROM productlines;
+ 
+ select firstName,jobTitle from employees;
+ 
+ 
+ /* 
+ to sort a result set use the MySQL ORDER BY clause.
+ he ORDER BY clause is always evaluated after the FROM and SELECT clause.
+ By default, the ORDER BY clause uses ASC
+ */
+ 
+ select customerNumber 
+ from customers
+ order by customerNumber;
+ 
+ 
+ select customerNumber
+ from customers
+ order by customerNumber asc;
+ 
+ select customerNumber,customerName,contactFirstName,city
+ from customers
+ order by customerName asc,contactFirstName desc;
+ 
+ select orderNumber, orderlinenumber,quantityOrdered * priceEach As subtotal
+ from orderdetails
+ order by subtotal Desc;
+ 
+ select orderNumber,status
+ from orders
+ order by
+ field(status,'In Process','on hold','Cancelled','Resolved','Dispached','Shipped');
+ 
+ 
+/* 
+The WHERE clause allows you to specify a search condition for the rows returned by a query
+Besides the SELECT statement, you can use the WHERE clause in the UPDATE or DELETE statement to specify which rows to update or delete.
+the WHERE clause is evaluated after the FROM clause and before the SELECT clause.
+or,and,like,between,in,limit
+MySQL always evaluates the OR operators after the AND operators. To change the order of evaluation,  use the parentheses
+When evaluating an expression that has the AND operator, MySQL stops evaluating the remaining parts of the expression 
+whenever it can determine the result. This function is called short-circuit evaluation
+expr [NOT] BETWEEN begin_expr AND end_expr;
+ All three expressions:  expr,  begin_expr, and  end_expr must have the same data type.
+ The BETWEEN operator returns true if the value of the  expr is greater than or equal to (>=) the value of  
+ begin_expr and less than or equal to (<= ) the value of the  end_expr, otherwise, it returns zero
+ The NOT BETWEEN returns true if the value of  expr is less than (<) the value of the  begin_expr or greater than the value of 
+ the value of  end_expr, otherwise, it returns 0.
+ If any expression is NULL, the BETWEEN operator returns  NULL .
+ 
+ The IN  operator allows you to determine if a specified value matches any value in a set of values or returned by a subquery.
+ */
+ 
