d12a895546e71c181436e4cc7c463e39af1e2111
pranjal9424 <pranjalgupta9424@gmail.com>
2020-02-19 12:57:48 +0530

    Inner self cross join

--- mysql.sql
+++ mysql.sql
@@ -124,3 +124,71 @@ from customers
 where state is not null
 order by state ,city;
 
+/* 
+If you use the GROUP BY clause in the SELECT statement without using aggregate functions, the GROUP BY clause behaves like the DISTINCT clause.
+The GROUP BY clause groups a set of rows into a set of summary rows 
+the GROUP BY clause returns unique occurrences of values.
+To filter the groups returned by GROUP BY clause, you use a  HAVING clause.
+ */    
+ 
+ select status from orders
+ group by status;
+ 
+ select * , count(*) as total
+ from orders
+ group by status order by total;
+ 
+select year(orderDate) as year, sum(quantityOrdered * priceEach) as total
+from orders Inner join orderdetails using (orderNumber)
+where status = 'Shipped'
+group by year
+having year > 2003; 
+
+ /* 
+ MySQL hasn’t supported the FULL OUTER JOIN yet.
+ The self join is often used to query hierarchical data or to compare a row with other rows within the same table.
+ The CROSS JOIN clause returns the Cartesian product of rows from the joined tables.
+ 
+ left,right,inner
+ */
+
+select t1.productCode,t1.productName,t2.textDescription
+from products t1 inner join productlines t2 on t1.productLine=t2.productLine; #on using for campare 
+
+select t1.productCode,t1.productName,t2.textDescription
+from products t1 inner join productlines t2 using(productLine);
+
+select status ,sum(quantityOrdered * priceEach) as amount
+from orders Inner join  orderdetails using(ordernumber)
+group by status;
+
+select orderNumber,orderDate,orderLineNumber,productName,quantityOrdered,priceEach
+from orders inner join orderdetails using(ordernumber) inner join products using (productCode)
+order by orderNumber,orderLineNumber;
+
+#self join
+select
+      concat(m.lastName,',',m.firstName) as manager,
+      concat(e.lastName,',',e.firstname) as 'direct report'
+      from
+      employees e
+      inner join employees m on
+      m.employeeNumber = e.reportsTo
+      order by manager;
+
+    
+
+SELECT 
+    IFNULL(CONCAT(m.lastname, ', ', m.firstname),'Top Manager') AS 'Manager',
+    CONCAT(e.lastname, ', ', e.firstname) AS 'Direct report'
+FROM
+    employees e
+LEFT JOIN employees m ON 
+    m.employeeNumber = e.reportsto
+ORDER BY 
+    manager DESC;
+    
+
+SELECT * FROM orders
+CROSS JOIN orderdetails;
+
